8cdfea21b8a13d7429163d99283dc298c3d1b517
Kauã <kauasimone.1@gmail.com>
2023-05-02 15:51:24 -0300

    alter

--- Perfil/PerfilArtista/UpdateUsuario.php
+++ Perfil/PerfilArtista/UpdateUsuario.php
@@ -2,7 +2,7 @@
         require_once 'GlobalPerfil.php';
         
         session_start();
-        header('Location: perfil.php');     
+        //header('Location: perfil.php');     
 
         $conexao = Conexao::conectar();
 
@@ -24,10 +24,29 @@
 
             $prepareStatement->execute();
 
+            if(isset($_SESSION['generoArtista'])){
+                $queryInsert2 = "UPDATE tbArtista
+                                SET generoArtista = ?
+                                ,bioArtista = ?
+                                WHERE idUsuario = ?";
+
+                $prepareStatement2 = $conexao->prepare($queryInsert2);
+
+                $prepareStatement2->bindValue(1, $_POST['generoArtista']);
+                $prepareStatement2->bindValue(2, $_POST['bioArtista']);
+                $prepareStatement2->bindValue(3, $_SESSION['idUsuario']);
+
+                $prepareStatement2->execute();
+
+            }
+
             $_SESSION['nomeUsuario'] = $_POST['nomeUsuario'];
             $_SESSION['nicknameUsuario'] = $_POST['nicknameUsuario'];
             $_SESSION['foneUsuario'] = $_POST['foneUsuario'];
             $_SESSION['estadoUsuario'] = $_POST['estadoUsuario'];
             $_SESSION['cidadeUsuario'] = $_POST['cidadeUsuario'];
+            $_SESSION['generoArtista'] = $_POST['generoArtista'];
+            $_SESSION['bioArtista'] = $_POST['bioArtista'];
+
 
 ?>
--- Perfil/PerfilArtista/perfil.php
+++ Perfil/PerfilArtista/perfil.php
@@ -314,13 +314,13 @@
                             <input type="text" class="form-control" aria-label="Sizing example input" name="bioArtista" value="<?PHP echo $_SESSION['bioArtista']; ?>" aria-describedby="inputGroup-sizing-default">
 
                         </div>
-                        <!--<label>Genero:</label>
+                        <label>Genero:</label>
                     <div class="input-group mb-3">
 
                         <input type="text" class="form-control" aria-label="Sizing example input" name="generoArtista"  value="<?PHP echo $_SESSION['generoArtista']; ?>"
                             aria-describedby="inputGroup-sizing-default">
 
-                    </div>-->
+                    </div>
                         <label>Telefone:</label>
                         <div class="input-group mb-3">
 
@@ -331,7 +331,7 @@
                         <div class="form-floating mb-3" style="display: flex;
                                           flex-direction: column;">
                            
-                            <select id="estado" name="estadoVisitante" class="select-estado">
+                            <select id="estado" name="estadoUsuario" class="select-estado">
                                 
                                 <option value="AC">Acre</option>
                                 <option value="AL">Alagoas</option>
@@ -369,14 +369,15 @@
                             <input type="text" class="form-control" aria-label="Sizing example input" name="cidadeUsuario" value="<?PHP echo $_SESSION['cidadeUsuario']; ?>" aria-describedby="inputGroup-sizing-default">
 
                         </div>
-                    </form>
+                    
 
 
                 </div>
-                <div class="modal-footer">
+                    <div class="modal-footer">
 
-                    <button type="button" class="btn btn-primary">Salvar</button>
-                </div>
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -508,19 +509,19 @@
                                 </div>
                                 <label>Bairro: </label>
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" name="" placeholder="bairro">
+                                    <input type="text" class="form-control" name="bairroEvento" placeholder="bairro">
                                 </div>
                                 <label>CEP: </label>
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder="cep">
+                                    <input type="text" class="form-control" name="cepEvento" placeholder="cep">
                                 </div>
                                 <label>Cidade: </label>
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder="cidade">
+                                    <input type="text" class="form-control" name="cidEvento" placeholder="cidade">
                                 </div>
                                 <label>Estado: </label>
                                 <div class="input-group mb-3">
-                                    <select id="estado-evento" name="estado-evento" class="select-estado">
+                                    <select id="estado-evento" name="estadoEvento" class="select-estado">
                                         <option value="AC">Selecionar</option>
                                         <option value="AC">Acre</option>
                                         <option value="AL">Alagoas</option>
@@ -559,12 +560,12 @@
                                         <label class="picture" for="picture__input" tabIndex="0">
                                             <span class="picture__image"></span>
                                         </label>
-                                        <input type="file" name="picture__input" id="picture__input">
+                                        <input type="file" name="imagemEvento" id="picture__input">
                                     </div>
                                 </div>
                                 <label>Descrição do evento</label>
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" id="desc-evento" placeholder="descrição">
+                                    <input type="textarea" class="form-control" name="descEvento" id="desc-evento" placeholder="descrição">
                                 </div>
                             </div>
                         </div>
