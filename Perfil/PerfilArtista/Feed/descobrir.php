bff4901ab3c1ca45a0b90e4b7c31930299eb2274
Kauã <kauasimone.1@gmail.com>
2023-05-08 17:37:59 -0300

    gdgdhr

--- Perfil/PerfilArtista/Feed/descobrir.php
+++ Perfil/PerfilArtista/Feed/descobrir.php
@@ -16,34 +16,40 @@
 
 <body>
     <div class="area-buscar">
-        <form name="FormBusca" id="FormBusca" method="Post" action="">
+        <form name="FormBusca" id="FormBusca" method="Post" action="descobrir.php">
             <input type="search" id="search" name="busca" placeholder="Pesquisar...">
             <button type="submit"><img src="assets/img/search.png"></i></button>
             
         </form>
         </div>
         <?php
-        if(isset($_POST['busca'])){
-            $conexao = Conexao::conectar();
-            $consulta = $conexao->prepare("SELECT * FROM tbUsuario WHERE nicknameUsuario LIKE ?");
-            $consulta->bindValue(1, '%' . $_POST['busca'] . '%');
-            $consulta->execute();
-            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-    
-            if (count($resultado) > 0) {
-                $html = '';
-                foreach ($resultado as $row) {
-                    $html .= '<li>';
-                    $html .= '<img src="../assets/img/FotoPerfil/' . $row['fotoPerfilUsuario'] . '" alt="Imagem de perfil">' ;
-                    $html .= '<a href="#">' . $row['nicknameUsuario'] . '</a>';
-                    $html .= '</li>';
-                }
-                echo '<ul id="results">' . $html . '</ul>';
-            } else {
-                echo '<ul id="results"><li>Nenhum resultado encontrado</li></ul>';
-            }
             
-         }?>
+            if(isset($_POST['busca'])){
+                $conexao = Conexao::conectar();
+                $consulta = $conexao->prepare("SELECT * FROM tbUsuario WHERE nicknameUsuario LIKE ?");
+                $consulta->bindValue(1, '%' . $_POST['busca'] . '%');
+                $consulta->execute();
+                $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        
+                if (count($resultado) > 0) {
+                    $html = '';
+                    foreach ($resultado as $row) {
+                        $html .= '<form action="../perfilMostrar.php" method="POST">';
+                        $html .= '<li>';
+                        $html .= '<img src="../assets/img/FotoPerfil/' . $row['fotoPerfilUsuario'] . '" alt="Imagem de perfil">' ;
+                        $html .= '<input type="hidden" name="usuarioNome" value= "'. $row['nomeUsuario'] .'">';
+                        $html .= '<input type="hidden" name="usuarioNick" value= "'. $row['nicknameUsuario'] .'">';
+                        $html .= '<button type="submit">' . $row['nicknameUsuario'] . '</button>';
+                        $html .= '</li>';
+                        $html .= '</form>';
+                    }
+                    echo '<ul id="results">' . $html . '</ul>';
+                } else {
+                    echo '<ul id="results"><li>Nenhum resultado encontrado</li></ul>';
+                }
+
+            }
+         ?>
         
      
     
