91807f74659afd83b8c3eb041acbb02b1ab56a24
GabAraujo08 <105180388+GabAraujo08@users.noreply.github.com>
2023-05-11 09:48:13 -0300

    09:48

--- Perfil/PerfilArtista/Feed/feed.php
+++ Perfil/PerfilArtista/Feed/feed.php
@@ -46,7 +46,7 @@ require_once '../../../Dao/Conexao.php';
                             <a href="../Feed/descobrir.php">
                                 <li class="list-group-item"><button id="descobrir" class="btn btn-primary btn-item-list" type="button">Descobrir</button></li>
                             </a>
-                            <a href="">
+                            <a href="../perfil.php">
                                 <li class="list-group-item"><button id="perfil" class="btn btn-primary btn-item-list" type="button">Perfil</button></li>
                             </a>
                         </ul>
