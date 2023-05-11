13c577d919ccf2404c166644a69b5e1b24f107eb
Gabriel Araujo <105180388+GabAraujo08@users.noreply.github.com>
2023-05-11 11:03:09 -0300

    10:49

--- Perfil/PerfilVisitante/perfil-visitante.php
+++ Perfil/PerfilVisitante/perfil-visitante.php
@@ -29,7 +29,7 @@ require_once 'GlobalPerfil.php';
                     </div>
                     <div class="d-flex justify-content-center align-items-center flex-column list-group-box">
                     <ul class="list-group">
-                            <a href="#">
+                            <a href="../PerfilVisitante/Feed/feed.php">
                                 <li class="list-group-item"><button id="inicio" type="button" class="btn btn-primary custom-btn-item">Início</button></li>
                             </a>
                             <a href="">
