e03c4c18f61b8e3157486e735d4c8c02386a97c7
GabAraujo08 <105180388+GabAraujo08@users.noreply.github.com>
2023-05-14 19:45:48 -0300

    Comentario e Denuncia de comentarios

--- Perfil/PerfilArtista/Feed/feed.php
+++ Perfil/PerfilArtista/Feed/feed.php
@@ -404,7 +404,7 @@ require_once '../../../Dao/Conexao.php';
                             <h1>@gabbs</h1>
                             <p>uctus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed non tellus auctor, consequat mi eu, pulvinar ipsum. Quisque vel ipsum eros. Nam consequat vestibulum ligula, sed iaculis quam. Sed nec ante velit. Nullam eget massa sit amet erat pharetra euismod sed id elit. Praesent a fringilla mauris. Fusce ut odio et elit laoreet fermentum. Nulla vel est ligula. Nam eget enim euismod, semper leo ac, congue justo. Maecenas nec nibh a arcu efficitur facilisis a ac lectus.</p>
                             <div class="box-btn-denuncia">
-                                <button id="myBtn" type="button"><i class="fa-solid fa-flag" style="color: #ef220b;"></i></button>
+                                <button data-bs-toggle="modal" data-bs-target="#denunciaModal" id="myBtn" type="button"><i class="fa-solid fa-flag" style="color: #ef220b;"></i></button>
                             </div>
                         </div>
                     </div>
@@ -524,7 +524,105 @@ require_once '../../../Dao/Conexao.php';
     </div>
 
 
-    
+    <div class="modal fade" id="denunciaModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Você está denunciando @gabbs</h1>
+
+
+
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <h1  style="font-family: 'InterBold';
+                        font-size: 18px;
+                        margin-bottom: 5px;">
+                        Este é o comentário que você quer denunciar?
+                    </h1>
+                    <div class="box-comentario ">
+                        <img src="assets/img/img-perfil.svg" alt="">
+                        <div class="conteudo-comentario">
+                            <h1>@gabbs</h1>
+                            <p>uctus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed non tellus auctor, consequat mi eu, pulvinar ipsum. Quisque vel ipsum eros. Nam consequat vestibulum ligula, sed iaculis quam. Sed nec ante velit. Nullam eget massa sit amet erat pharetra euismod sed id elit. Praesent a fringilla mauris. Fusce ut odio et elit laoreet fermentum. Nulla vel est ligula. Nam eget enim euismod, semper leo ac, congue justo. Maecenas nec nibh a arcu efficitur facilisis a ac lectus.</p>
+
+                        </div>
+                    </div>
+
+                    <h1  style="font-family: 'InterBold';
+                        font-size: 18px;
+                        margin-bottom: 5px;">
+                       Selecione o que mais se assemelha com sua denúncia!
+                    </h1>
+
+                    <select  name="tipoDenuncia" id="">
+                        <option value="#">Violência</option>
+                        <option value="#">Assédio</option>
+                        <option value="#">Preconceito</option>
+                        <option value="#">Discurso de ódio</option>
+                        <option value="#">Mensagem falsa</option>
+
+                    </select>
+
+                    <h1  style="font-family: 'InterBold';
+                        font-size: 18px;
+                        margin-top: 5px;">
+                       Abaixo faça uma breve descrição sobre sua denúncia!
+                    </h1>
+
+
+
+
+
+
+
+
+                </div>
+                <div class="modal-footer">
+                    <div id="divComentario" class="comentario slide-in">
+
+                        <div class="box-text-area">
+                            <form action="#">
+                                <textarea placeholder="Escreva sua denúncia" name="comentario" id="" cols="30" rows="10">
+
+                            </textarea>
+                                <div class="box-btn-comentario">
+                                    <button class="btn btn-primary">
+                                        <i class="fa-solid fa-paper-plane fa-lg" style="color: #000000;"></i>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+
+
+
+
+
+                    </div>
+                    <!-- <div id="divDenuncia" style="display: none;" class="comentario slide-in">
+                        <div class="box-text-area">
+                            <form action="#">
+                                <textarea placeholder="Qual motivo da sua denúncia?" name="" id="" cols="30" rows="10">
+
+                            </textarea>
+                                <div class="box-btn-comentario">
+                                    <button class="btn btn-primary">
+                                        <i class="fa-solid fa-paper-plane fa-lg" style="color: #ef220b;"></i>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div> -->
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 
 
 
@@ -534,40 +632,10 @@ require_once '../../../Dao/Conexao.php';
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
-    </script> -->
-    <!-- 
-   
-
-    <script>
-        const btnMostrarComentario = document.getElementById('btnComentario');
-
-        const divComentario = document.getElementById('divComentario');
 
 
-btnMostrarComentario.addEventListener('click', function() {
-  divComentario.style.display = 'block';
-});
-
-    </script> -->
-
-    <!-- <script>
-        const btnDenuncia = document.getElementById('btn-denuncia');
-
-        const divComentario = document.getElementById('divComentario');
-        const divDenuncia = document.getElementById('divDenuncia');
 
 
-        btnDenuncia.addEventListener('click', function() {
-            divComentario.style.display = 'none';
-
-            divDenuncia.style.display = 'block';
-        });
-    </script> -->
 </body>
 
 </html>
