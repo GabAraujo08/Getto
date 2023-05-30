65b0ad4f2e1b4e27304ac07973f1fbe1891bb578
LualuaSantos <132376550+LualuaSantos@users.noreply.github.com>
2023-05-30 15:53:28 -0300

    dfdsfgdgdsfdsfgdgdfgdv

--- Controller/Deseguir.php
+++ Controller/Deseguir.php
@@ -1,10 +1,11 @@
 <?php
     require_once 'GlobalController.php';
 
+    session_start();
 
     $conexao = Conexao::conectar();
     $consulta = $conexao->prepare('DELETE FROM tbSeguidores WHERE idUsuario = ? AND idArtista = ?');
-    $consulta->bindValue(1, $_POST['idUsuario']);
+    $consulta->bindValue(1, $_SESSION['idUsuario']);
     $consulta->bindValue(2, $_POST['idArtista']);
     $consulta->execute();
 
