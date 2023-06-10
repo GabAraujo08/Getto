e977393d1045ac0c0d64ed6c37501fd7cadc6657
GabAraujo08 <105180388+GabAraujo08@users.noreply.github.com>
2023-06-10 00:03:05 -0300

    00:02

--- Perfil/PerfilArtista/Feed/descobrir.php
+++ Perfil/PerfilArtista/Feed/descobrir.php
@@ -14,8 +14,9 @@ error_reporting(0);
     <title>Getto</title>
     <link rel="shortcut icon" href="assets/img/logomarca.png" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/feedMobile.css">
+    
     <link rel="stylesheet" href="assets/css/descobrir.css">
+    <link rel="stylesheet" href="assets/css/feedMobile.css">
     <link rel="stylesheet" href="../../../assets/fontawesome/css/all.min.css">
 
 
