ec9156fa34c92ae7dd96226cd95084fb9ebd67eb
Gabriel Araujo <129636599+joaoporfirio1@users.noreply.github.com>
2023-06-15 14:26:21 -0300

    GYIGIYGI

--- Perfil/PerfilArtista/Evento/eventoArtista.php
+++ Perfil/PerfilArtista/Evento/eventoArtista.php
@@ -174,7 +174,7 @@ color: #656565;">
                 <?php
                 require_once '../../../Dao/EventoDao.php';
 
-                $eventos = EventoDao::ListaEvento($_SESSION['idUsuario']);
+                $eventos = EventoDao::ListaEvento();
                 foreach ($eventos as $index => $evento) { ?>
 
 
@@ -209,7 +209,7 @@ color: #656565;">
                                 <div class="accordion-body rounded-4">
                                     <div class="baixo">
                                         <div class="img-evento">
-                                            <img src="assets/img/<?php echo $evento['imagemEvento'] ?> ">
+                                            <img src="assets/img/<?php echo $evento['imagemEvento']; ?>">
                                         </div>
                                         <div class="conteudo-evento">
                                             <div class="criador-evento">
