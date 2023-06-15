f88d747eaa2dc0d1a4ba566b3f4bc36b001581f6
melzitasz <35741475+melzitasz@users.noreply.github.com>
2023-06-14 22:46:14 -0300

    finalizacao do back-end da rigth-bar

--- Dao/ArtistaDao.php
+++ Dao/ArtistaDao.php
@@ -1,71 +1,95 @@
 <?php
-    require_once 'GlobalDao.php';
-    class ArtistaDao{
-        public static function cadastrar($artista){
-            $conexao = Conexao::conectar();
+require_once 'GlobalDao.php';
+class ArtistaDao
+{
+    public static function cadastrar($artista)
+    {
+        $conexao = Conexao::conectar();
 
-            $queryInsert = "INSERT INTO tbArtista(generoArtista, bioArtista, portfolio, idUsuario)
+        $queryInsert = "INSERT INTO tbArtista(generoArtista, bioArtista, portfolio, idUsuario)
                             VALUES(?, ?, ?, ?)";
-            
-            $prepareStatement = $conexao->prepare($queryInsert);
-            
-            $prepareStatement->bindValue(1, $artista->getGeneroArtista());
-            $prepareStatement->bindValue(2, $artista->getBioArtista());
-            $prepareStatement->bindValue(3, $artista->getPortfolioArtista());
-            $prepareStatement->bindValue(4, $artista->getUsuario());
-            $prepareStatement->execute();
-            return 'Cadastrou';
-        }
 
-        public static function atualizarBioArtista($artista){
-            $conexao = Conexao::conectar();
+        $prepareStatement = $conexao->prepare($queryInsert);
 
-            $queryInsert = "UPDATE tbArtista
+        $prepareStatement->bindValue(1, $artista->getGeneroArtista());
+        $prepareStatement->bindValue(2, $artista->getBioArtista());
+        $prepareStatement->bindValue(3, $artista->getPortfolioArtista());
+        $prepareStatement->bindValue(4, $artista->getUsuario());
+        $prepareStatement->execute();
+        return 'Cadastrou';
+    }
+
+    public static function atualizarBioArtista($artista)
+    {
+        $conexao = Conexao::conectar();
+
+        $queryInsert = "UPDATE tbArtista
                             SET bioArtista = ?
                             WHERE idArtista = ?";
-            
-            $prepareStatement = $conexao->prepare($queryInsert);
-            
-            $prepareStatement->bindValue(1, $artista->getBioArtista());
-            $prepareStatement->bindValue(2, $artista->getIdArtista());
 
-            $prepareStatement->execute();
-            return 'Atualizou';
-        }
+        $prepareStatement = $conexao->prepare($queryInsert);
 
-        public static function atualizarGeneroArtista($artista){
-            $conexao = Conexao::conectar();
+        $prepareStatement->bindValue(1, $artista->getBioArtista());
+        $prepareStatement->bindValue(2, $artista->getIdArtista());
 
-            $queryInsert = "UPDATE tbArtista
+        $prepareStatement->execute();
+        return 'Atualizou';
+    }
+
+    public static function atualizarGeneroArtista($artista)
+    {
+        $conexao = Conexao::conectar();
+
+        $queryInsert = "UPDATE tbArtista
                             SET generoArtista = ?
                             WHERE idArtista = ?";
-            
-            $prepareStatement = $conexao->prepare($queryInsert);
-            
-            $prepareStatement->bindValue(1, $artista->getGeneroArtista());
-            $prepareStatement->bindValue(2, $artista->getIdArtista());
 
-            $prepareStatement->execute();
-            return 'Atualizou';
-        }
+        $prepareStatement = $conexao->prepare($queryInsert);
 
-        public static function consultarArtista($artista){
-            $conexao = Conexao::conectar();
-            $consulta = $conexao->prepare('SELECT * FROM tbArtista WHERE idUsuario = ?');
-            $consulta->bindValue(1, $artista);
-            $consulta->execute();
-            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
-            
-            return $resultado;
-        }
+        $prepareStatement->bindValue(1, $artista->getGeneroArtista());
+        $prepareStatement->bindValue(2, $artista->getIdArtista());
 
-        public static function deletar($artista){
-            $conexao = Conexao::conectar();
-            $delete = $conexao->prepare("DELETE FROM tbArtista WHERE idArtista = ?");
-            $delete->bindValue(1, $artista);
-            $delete->execute();
-
-            return 'Deletou';
-        }
+        $prepareStatement->execute();
+        return 'Atualizou';
     }
-?>
+
+    public static function consultarArtista($artista)
+    {
+        $conexao = Conexao::conectar();
+        $consulta = $conexao->prepare('SELECT * FROM tbArtista WHERE idUsuario = ?');
+        $consulta->bindValue(1, $artista);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+
+        return $resultado;
+    }
+
+    public static function deletar($artista)
+    {
+        $conexao = Conexao::conectar();
+        $delete = $conexao->prepare("DELETE FROM tbArtista WHERE idArtista = ?");
+        $delete->bindValue(1, $artista);
+        $delete->execute();
+
+        return 'Deletou';
+    }
+
+    public static function buscaArtistasAtivos()
+    {
+        $conexao = Conexao::conectar();
+        $consulta = $conexao->prepare('
+        select u.*, a.bioArtista, a.idArtista, count(p.idPublicacao) as qtdPosts from tbpublicacao p
+        join tbartista a ON
+        p.idArtista = a.idArtista
+        join tbusuario u ON
+        a.idUsuario = u.idUsuario
+        group by p.idArtista
+        order by count(p.idPublicacao) desc
+                ');
+
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        return $resultado;
+    }
+}
--- Dao/publicacaoDao.php
+++ Dao/publicacaoDao.php
@@ -1,36 +1,40 @@
 <?PHP
-    require_once 'GlobalDao.php';
+require_once 'GlobalDao.php';
 
-    class PublicacaoDao{
+class PublicacaoDao
+{
 
-        public static function cadastrar($publicacao){
-            $conexao = Conexao::conectar();
+    public static function cadastrar($publicacao)
+    {
+        $conexao = Conexao::conectar();
 
-            $queryInsert = "INSERT INTO tbPublicacao(idArtista, descPublicacao, statusPublicacao, idTipoArte)
+        $queryInsert = "INSERT INTO tbPublicacao(idArtista, descPublicacao, statusPublicacao, idTipoArte)
                             VALUES(?, ?, ?, ?)";
-            
-            $prepareStatement = $conexao->prepare($queryInsert);
-            
-            $prepareStatement->bindValue(1, $publicacao->getIdArtista());
-            $prepareStatement->bindValue(2, $publicacao->getDescPublicacao());
-            $prepareStatement->bindValue(3, $publicacao->getStatusPublicacao());
-            $prepareStatement->bindValue(4, $publicacao->getIdTipoArte());
-            $prepareStatement->execute();
-            return 'Cadastrou'; 
-        }
-        public static function consultarId($publicacao){
-            $conexao = Conexao::conectar();
-            $querySelect = "SELECT idPublicacao FROM tbPublicacao WHERE descPublicacao LIKE '".$publicacao->getDescPublicacao()."'";
-            $resultado = $conexao->query($querySelect);
-            $lista = $resultado->fetchAll();
-            foreach ($lista as $publicacao)
-                $id = $publicacao['idPublicacao'];
-            return $id;   
-        }
-        public static function ListaPublicacao(){
-            $conexao = Conexao::conectar();
-            
-            $consulta = $conexao->prepare('SELECT tbUsuario.nicknameUsuario,  tbUsuario.fotoPerfilUsuario, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao
+
+        $prepareStatement = $conexao->prepare($queryInsert);
+
+        $prepareStatement->bindValue(1, $publicacao->getIdArtista());
+        $prepareStatement->bindValue(2, $publicacao->getDescPublicacao());
+        $prepareStatement->bindValue(3, $publicacao->getStatusPublicacao());
+        $prepareStatement->bindValue(4, $publicacao->getIdTipoArte());
+        $prepareStatement->execute();
+        return 'Cadastrou';
+    }
+    public static function consultarId($publicacao)
+    {
+        $conexao = Conexao::conectar();
+        $querySelect = "SELECT idPublicacao FROM tbPublicacao WHERE descPublicacao LIKE '" . $publicacao->getDescPublicacao() . "'";
+        $resultado = $conexao->query($querySelect);
+        $lista = $resultado->fetchAll();
+        foreach ($lista as $publicacao)
+            $id = $publicacao['idPublicacao'];
+        return $id;
+    }
+    public static function ListaPublicacao()
+    {
+        $conexao = Conexao::conectar();
+
+        $consulta = $conexao->prepare('SELECT tbUsuario.nicknameUsuario,  tbUsuario.fotoPerfilUsuario, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao
                                            FROM tbPublicacao
                                            INNER JOIN tbArtista ON tbArtista.idArtista = tbPublicacao.idArtista
                                            INNER JOIN tbUsuario ON tbUsuario.idUsuario = tbArtista.idUsuario
@@ -41,17 +45,18 @@
                                               FROM tbPublicacao
                                               WHERE tbPublicacao.idArtista = tbArtista.idArtista
                                            ) DESC, tbPublicacao.horarioPublicacao DESC');
-            
-            $consulta->execute();
-            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-            
-            return $resultado;
-        }
-        
-        public static function ListaMinhasPublicacao($id){
-            
-            $conexao = Conexao::conectar();
-            $consulta = $conexao->prepare('SELECT DISTINCT tbUsuario.nicknameUsuario,  tbUsuario.fotoPerfilUsuario, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao FROM tbPublicacao
+
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        return $resultado;
+    }
+
+    public static function ListaMinhasPublicacao($id)
+    {
+
+        $conexao = Conexao::conectar();
+        $consulta = $conexao->prepare('SELECT DISTINCT tbUsuario.nicknameUsuario,  tbUsuario.fotoPerfilUsuario, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao FROM tbPublicacao
                                                INNER JOIN tbArtista ON tbArtista.idArtista = tbPublicacao.idArtista
                                                INNER JOIN tbUsuario ON tbUsuario.idUsuario = tbArtista.idUsuario
                                                INNER JOIN tbMidiaPublicacao ON tbMidiaPublicacao.idPublicacao = tbPublicacao.idPublicacao
@@ -60,28 +65,29 @@
                                                WHERE tbPublicacao.idArtista = ?
                                                ORDER BY tbPublicacao.horarioPublicacao DESC
                                                ');
-            $consulta->bindValue(1, $id);
-            $consulta->execute();
-            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-            
-            return $resultado;
-        }
-        public static function ListaPublicacaoSegui($id){
-            
-            $conexao = Conexao::conectar();
+        $consulta->bindValue(1, $id);
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        return $resultado;
+    }
+    public static function ListaPublicacaoSegui($id)
+    {
+
+        $conexao = Conexao::conectar();
 
 
-            $query = $conexao->prepare('SELECT idArtista FROM tbSeguidores WHERE idUsuario =?');
-            $query->bindValue(1, $id);
-             $query->execute();
-            $resultado1 = $query->fetchAll(PDO::FETCH_ASSOC);
+        $query = $conexao->prepare('SELECT idArtista FROM tbSeguidores WHERE idUsuario =?');
+        $query->bindValue(1, $id);
+        $query->execute();
+        $resultado1 = $query->fetchAll(PDO::FETCH_ASSOC);
 
-            
-            $countResu1 = count($resultado1);
-            $resultado = [];
 
-            for($i = 0; $i < $countResu1; $i ++){
-                $consulta = $conexao->prepare('SELECT tbUsuario.nicknameUsuario,  tbUsuario.fotoPerfilUsuario, tbPublicacao.idPublicacao, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao FROM tbPublicacao
+        $countResu1 = count($resultado1);
+        $resultado = [];
+
+        for ($i = 0; $i < $countResu1; $i++) {
+            $consulta = $conexao->prepare('SELECT tbUsuario.nicknameUsuario,  tbUsuario.fotoPerfilUsuario, tbPublicacao.idPublicacao, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao FROM tbPublicacao
                                 INNER JOIN tbArtista ON tbArtista.idArtista = tbPublicacao.idArtista
                                 INNER JOIN tbUsuario ON tbUsuario.idUsuario = tbArtista.idUsuario
                                 INNER JOIN tbMidiaPublicacao ON tbMidiaPublicacao.idPublicacao = tbPublicacao.idPublicacao
@@ -89,45 +95,47 @@
                                 WHERE tbPublicacao.idArtista = ?
                                 ORDER BY tbPublicacao.horarioPublicacao DESC
                                 ');
-                $consulta->bindValue(1, $resultado1[$i]['idArtista']);
-                $consulta->execute();
-                $resultado2 = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            $consulta->bindValue(1, $resultado1[$i]['idArtista']);
+            $consulta->execute();
+            $resultado2 = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
-                $resultado = array_merge($resultado, $resultado2);
-            }
-            return $resultado;
-           
+            $resultado = array_merge($resultado, $resultado2);
         }
+        return $resultado;
+    }
 
-        public static function QuantPublis($id){
-            $conexao = Conexao::conectar();
+    public static function QuantPublis($id)
+    {
+        $conexao = Conexao::conectar();
 
-            $query = $conexao->prepare('SELECT COUNT(idPublicacao) as quantPubli FROM tbPublicacao WHERE idArtista = ?');
-            $query->bindValue(1, $id);
-             $query->execute();
-            $resultado1 = $query->fetchAll(PDO::FETCH_ASSOC);
+        $query = $conexao->prepare('SELECT COUNT(idPublicacao) as quantPubli FROM tbPublicacao WHERE idArtista = ?');
+        $query->bindValue(1, $id);
+        $query->execute();
+        $resultado1 = $query->fetchAll(PDO::FETCH_ASSOC);
 
-            return $resultado1;
-        }
+        return $resultado1;
+    }
 
-        public static function ListaPublicacaoCurti($id){
-            $conexao = Conexao::conectar();
-            $query = $conexao->prepare('SELECT tbPublicacao.idPublicacao, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao FROM tbCurtida
+    public static function ListaPublicacaoCurti($id)
+    {
+        $conexao = Conexao::conectar();
+        $query = $conexao->prepare('SELECT tbPublicacao.idPublicacao, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao FROM tbCurtida
                 INNER JOIN tbPublicacao ON tbCurtida.idPublicacao = tbPublicacao.idPublicacao
                 INNER JOIN tbMidiaPublicacao ON tbMidiaPublicacao.idPublicacao = tbPublicacao.idPublicacao
                 INNER JOIN tbMidia ON tbMidiaPublicacao.idMidia = tbMidia.idMidia
                 WHERE tbCurtida.idUsuario = ?
                 ORDER BY tbPublicacao.horarioPublicacao DESC');
-            $query->bindValue(1, $id);
-            $query->execute();
-            $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
-        
-            return $resultado;
-        }
+        $query->bindValue(1, $id);
+        $query->execute();
+        $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
 
-        public static function TipoArteComMaisPublicacoes(){
-            $conexao = Conexao::conectar();
-            $consulta = $conexao->prepare('SELECT DISTINCT tbPublicacao.idPublicacao, tbUsuario.nicknameUsuario, tbUsuario.fotoPerfilUsuario, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, tbTipoArte.idTipoArte, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao 
+        return $resultado;
+    }
+
+    public static function TipoArteComMaisPublicacoes()
+    {
+        $conexao = Conexao::conectar();
+        $consulta = $conexao->prepare('SELECT DISTINCT tbPublicacao.idPublicacao, tbUsuario.nicknameUsuario, tbUsuario.fotoPerfilUsuario, tbPublicacao.descPublicacao, tbMidia.arquivoMidia, tbTipoArte.idTipoArte, TIMESTAMPDIFF(MINUTE, tbPublicacao.horarioPublicacao, NOW()) as minutosPublicacao 
                                           FROM tbPublicacao
                                           INNER JOIN tbArtista ON tbArtista.idArtista = tbPublicacao.idArtista
                                           INNER JOIN tbTipoArte ON tbTipoArte.idTipoArte = tbPublicacao.idTipoArte
@@ -141,24 +149,25 @@
                                               ORDER BY numPublicacoes DESC
                                           ) AS tbPublicacoesContagem ON tbPublicacoesContagem.idTipoArte = tbTipoArte.idTipoArte
                                           ORDER BY tbPublicacoesContagem.numPublicacoes DESC, tbPublicacao.horarioPublicacao DESC');
-            $consulta->execute();
-            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-            
-            return $resultado;
-        }
-        public static function buscaTopCategorias(){
-            $conexao = Conexao::conectar();
- 
-            $consulta = $conexao->prepare('
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        return $resultado;
+    }
+    public static function buscaTopCategorias()
+    {
+        $conexao = Conexao::conectar();
+
+        $consulta = $conexao->prepare('
             select ta.nomeTipoArte as nomeTipoArte, count(p.idTipoArte) as contagemCaregorias from tbpublicacao p
             join tbtipoarte ta on p.idTipoArte = ta.idTipoArte
             group by ta.nomeTipoArte
             order by count(p.idTipoArte) desc
             ');
 
-            $consulta->execute();
-            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-            
-            return $resultado;
-        }
-} 
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        return $resultado;
+    }
+}
