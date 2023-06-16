813218d64c2162601bb7b947b7673136eabc41fe
Kauã <105180388+GabAraujo08@users.noreply.github.com>
2023-06-16 16:30:43 -0300

    thtytrhtrhtrht

--- Perfil/PerfilArtista/perfil.php
+++ Perfil/PerfilArtista/perfil.php
@@ -268,20 +268,20 @@ require_once 'GlobalPerfil.php';
                                     if ($p['idTipoMidia'] == 3) {
                                     ?>
                                         <img src="assets/img/Pubs/<?PHP echo $p['arquivoMidia']; ?>" alt="" class="img-publicacao"> <?PHP
-                                                                                                                                    } else if ($p['idTipoMidia'] == 2) {
-                                                                                                                                        ?>
+                                    } else if ($p['idTipoMidia'] == 2) {
+                                    ?>
                                         <video id="player-video" controls>
                                             <source src="assets/img/Pubs/<?PHP echo $p['arquivoMidia']; ?>">
                                         </video>
                                     <?PHP
-                                                                                                                                    } else {
+                                    } else {
                                     ?>
 
                                         <audio id="player-audio" controls>
                                             <source src="assets/img/Pubs/<?PHP echo $p['arquivoMidia']; ?>">
                                         </audio>
                                     <?PHP
-                                                                                                                                    }
+                                    }
                                     
                                 }
                                 ?>
