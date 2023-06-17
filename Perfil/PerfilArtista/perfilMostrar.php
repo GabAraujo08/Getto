e5514637fbf43fdef013d25c568bedca7e9b7dbf
CamillySouza20 <132386902+CamillySouza20@users.noreply.github.com>
2023-06-17 14:06:27 -0300

    xZx

--- Perfil/PerfilArtista/perfilMostrar.php
+++ Perfil/PerfilArtista/perfilMostrar.php
@@ -430,7 +430,47 @@ require_once 'GlobalPerfil.php';
         </div>
     </div>
 
-    
+    <div class="modal fade" id="adicionarPix" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="../../Controller/CadastraConta.php" method="Post" name="contapix" id="contapix">
+                        <h1>Adicione suas informações para doação!</h1>
+                        <div class="input-group mb-3">
+
+                            <input placeholder="Nome da conta" name="nomeContaPixArtista" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                        </div>
+
+                        <div class="input-group mb-3">
+
+                            <select name="tipoContaPixArtista" id="">
+                                <option value="CPF">CPF</option>
+                                <option value="CNPJ">CNPJ</option>
+                                <option value="TELEFONE">TELEFONE</option>
+                                <option value="EMAIL">EMAIL</option>
+                                <option value="ALEATORIA">ALEATORIA</option>
+                            </select>
+                        </div>
+
+                        <div class="input-group mb-3">
+
+                            <input placeholder="Chave PIX" name="chaveContaPixArtista" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                        </div>
+
+
+                </div>
+                <div class="modal-footer">
+
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
